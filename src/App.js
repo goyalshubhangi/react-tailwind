770c22946e7e504d648307d1be86e973e3d60f07
shubh_hp <goyalshubhangi72@gmail.com>
2023-08-19 22:33:12 +0530

    added all the components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./assets/main.css";
+import Appheader from "./components/Appheader";
+import Tabs from "./components/Tabs";
+import Timeline from "./components/Timeline";
 
 function App() {
+  const [activeTab, setActiveTab] = useState("allActivities");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-[770px] mx-auto ps-5 pe-4">
+      <Appheader />
+      <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
+      <Timeline activeTab={activeTab} />
     </div>
   );
 }
-
 export default App;
